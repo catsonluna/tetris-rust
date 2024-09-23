50f1a551e9c1c2415545c265e9fd411719e619e2
catsonluna <pinkulumc@gmail.com>
2024-09-23 13:44:42 +0300

    imrpoved ui rendering

--- src/engine/events/events.rs
+++ src/engine/events/events.rs
@@ -1,6 +1,6 @@
 use once_cell::sync::Lazy;
 
-use super::base::event::BaseEvent;
+use super::types::base::BaseEvent;
 
 pub static UPDATE_EVENT: Lazy<BaseEvent> = Lazy::new(|| BaseEvent::new());
 pub static TICK_EVENT: Lazy<BaseEvent> = Lazy::new(|| BaseEvent::new());
--- src/engine/events/mod.rs
+++ src/engine/events/mod.rs
@@ -1,3 +1,3 @@
 pub mod events;
 
-pub mod base;
+pub mod types;
